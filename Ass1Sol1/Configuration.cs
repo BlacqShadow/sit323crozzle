c1e746b694e1756e537706349940cd3f0d5e3840
RUDRA KUMAR <rudra@deakin.edu.au>
2017-09-02 03:00:01 +1000

    5 Out of 16 configurations checks complete; validation of file broken

--- Ass1Sol1/Configuration.cs
+++ Ass1Sol1/Configuration.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Ass1Sol1
@@ -9,7 +10,16 @@ namespace Ass1Sol1
     class Configuration
     {
 
-        private string[] data; 
+        private string[] data;
+        public bool isValid;
+        private bool invalidCrozzleScoreExists = false;
+        private const string logFilePattern = @"^\s*LOGFILE_NAME="".+""\s*$";
+        private const string minUniqueWordsPattern = @"MINIMUM_NUMBER_OF_UNIQUE_WORDS=\s*\d+";
+        private const string maxUniqueWordsPattern = @"MAXIMUM_NUMBER_OF_UNIQUE_WORDS=\s*\d+";
+        private const string invalidCrozzleScorePattern = @"\s*INVALID_CROZZLE_SCORE=""INVALID CROZZLE""\s*";
+        public uint MINIMUM_NUMBER_OF_UNIQUE_WORDS = 0;
+        public uint MAXIMUM_NUMBER_OF_UNIQUE_WORDS = 0;
+
         public Configuration(string configurationFile)
         {
             try
@@ -20,6 +30,39 @@ namespace Ass1Sol1
             {
                 Log.Error("Config File", string.Format("Unable to open file error: {0}", e.Message));
             }
+
+            isValid = true;
+            extractData();
         }
+
+        private void extractData()
+        {
+            foreach (string l in data)
+            {
+                //SRS #2 Check if the logfile name exists
+                if(Regex.IsMatch(l,logFilePattern))
+                {
+                    isValid = true;
+                }
+
+                if(Regex.IsMatch(l,minUniqueWordsPattern))
+                {
+                    isValid = true;
+                    //Extract the number from the string
+                    MINIMUM_NUMBER_OF_UNIQUE_WORDS = UInt32.Parse(Regex.Match(l, @"\d+").Value);
+                }
+                if (Regex.IsMatch(l, maxUniqueWordsPattern))
+                {
+                    isValid = true;
+                    //Extract the number from the string
+                    MAXIMUM_NUMBER_OF_UNIQUE_WORDS = UInt32.Parse(Regex.Match(l, @"\d+").Value);
+                }
+                if (Regex.IsMatch(l, invalidCrozzleScorePattern))
+                {
+                    invalidCrozzleScoreExists = true;
+                }
+            }
+        }
+
     }
 }
